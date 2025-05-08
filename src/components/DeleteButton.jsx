dbeea8add5147e8bffe34353b1d4ce05b34818ae
Bruno DaSilva <hi.dasilvab@gmail.com>
2025-05-08 15:39:54 -0500

    Added db deletion method tested it work properly

--- src/components/DeleteButton.jsx
+++ src/components/DeleteButton.jsx
@@ -1,8 +1,9 @@
 import Trash from "../icons/Trash";
-// import { db } from "../appwrite/databases";
+import { db } from "../appwrite/databases";
 
 const DeleteButton = ({ noteId, setNotes }) => {
   const handleDelete = async () => {
+    db.notes.delete(noteId);
     setNotes((prevState) => prevState.filter((note) => note.$id !== noteId));
   };
 
